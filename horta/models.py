2d8b21bf050ff2f78c7263187a783b9481143bf5
Eduan <eduanlenine@yahoo.com.br>
2016-02-13 11:20:10 -0200

    Renaming class and fields

--- horta/models.py
+++ horta/models.py
@@ -2,20 +2,20 @@ from __future__ import unicode_literals
 from django.db import models
 import datetime
 
-class Dificuldade(models.Model):
-    descricao = models.CharField(max_length=30)
+class Difficulty(models.Model):
+    description = models.CharField(max_length=30)
 
-class Clima(models.Model):
-    descricao = models.CharField(max_length=30)
+class Weather(models.Model):
+    description = models.CharField(max_length=30)
 
-class Planta(models.Model):
-    nome = models.CharField(max_length=100)
-    descricao = models.TextField()
-    min_temperatura = models.IntegerField()
-    max_temperatura = models.IntegerField()
-    min_dias_cultivo = models.IntegerField()
-    max_dias_cultivo = models.IntegerField()
-    dificuldade = models.ForeignKey(Dificuldade)
-    clima = models.ForeignKey(Clima)
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    min_temperature = models.IntegerField()
+    max_temperature = models.IntegerField()
+    min_days_cultivation = models.IntegerField()
+    max_days_cultivation = models.IntegerField()
+    difficulty = models.ForeignKey(Difficulty)
+    weather = models.ForeignKey(Weather)
     created_at = models.DateTimeField('Criado em', default = datetime.datetime.now)
     updated_at = models.DateTimeField('Atualizado em', default = datetime.datetime.now) 
