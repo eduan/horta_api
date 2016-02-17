504fa6581077c3eaf985175ef54e82608ea38f38
Eduan <eduanlenine@yahoo.com.br>
2016-02-16 23:24:18 -0200

    Creating conditions classes

--- horta/admin.py
+++ horta/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Plant, Condition, ConditionType, ConditionInterval
 
-# Register your models here.
+admin.site.register(Plant)
+admin.site.register(Condition)
+admin.site.register(ConditionType)
+admin.site.register(ConditionInterval)
--- horta/models.py
+++ horta/models.py
@@ -2,20 +2,37 @@ from __future__ import unicode_literals
 from django.db import models
 import datetime
 
-class Difficulty(models.Model):
-    description = models.CharField(max_length=30)
+#Weather, Difficulty, Light
+from django.utils.encoding import python_2_unicode_compatible
 
-class Weather(models.Model):
+@python_2_unicode_compatible
+class ConditionType(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible
+class Condition(models.Model):
     description = models.CharField(max_length=30)
+    condition_type = models.ForeignKey(ConditionType)
+    def  __str__(self):
+        return self.condition_type.name + ': ' + self.description
+
+@python_2_unicode_compatible
+class ConditionInterval(models.Model):
+    min = models.IntegerField(default=0)
+    max = models.IntegerField(default=0)
+    condition_type = models.ForeignKey(ConditionType)
+    def  __str__(self):
+        return self.condition_type.name + ' min: ' + str(self.min) + ' max: ' + str(self.max)
 
 class Plant(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    min_temperature = models.IntegerField()
-    max_temperature = models.IntegerField()
-    min_days_cultivation = models.IntegerField()
-    max_days_cultivation = models.IntegerField()
-    difficulty = models.ForeignKey(Difficulty)
-    weather = models.ForeignKey(Weather)
+    condition = models.ManyToManyField(Condition, related_name='%(class)s_condition')
+    temperature = models.ForeignKey(ConditionInterval, related_name='%(class)s_temp', default='');
+    days = models.ForeignKey(ConditionInterval, related_name='%(class)s_days', default='');
+    ph = models.ForeignKey(ConditionInterval, related_name='%(class)s_ph', default='');
     created_at = models.DateTimeField('Criado em', default = datetime.datetime.now)
-    updated_at = models.DateTimeField('Atualizado em', default = datetime.datetime.now) 
+    updated_at = models.DateTimeField('Atualizado em', default = datetime.datetime.now)
